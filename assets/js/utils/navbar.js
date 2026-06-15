2e923592c92bc0e7311ea98d351af874ad51461a
shankark1984 <46882965+shankark1984@users.noreply.github.com>
2026-06-15 12:19:02 +0530

    ok

--- assets/js/utils/navbar.js
+++ assets/js/utils/navbar.js
@@ -53,7 +53,7 @@ const Navbar = (() => {
                 { label: "Payment Receivable", icon: "bi-cash-coin", href: "#" },
                 { label: "Payment Payable", icon: "bi-currency-rupee", href: "#" },
                 { label: "Tax Details", icon: "bi-percent", href: "/pages/Reports/reportGSTDetails.html" },
-                { label: "Accounting Ledger", icon: "bi-journal-bookmark", href: "#" }
+                { label: "Accounting Ledger", icon: "bi-journal-bookmark", href: "/pages/Reports/accountingLedger.html" }
             ]
         },
         {
